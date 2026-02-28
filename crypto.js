a09261be6cd9bf515c0dd06e302e7f507e3329fa
AMENAYO <AMENAYO@proton.me>
2026-02-28 12:55:42 +0100

    Update crypto.js

--- crypto.js
+++ crypto.js
@@ -2,13 +2,18 @@ import CryptoJS from "crypto-js";
 
 const SECRET = "neo_ultra_secret";
 
-// Encrypt / Chiffrer
+// AES encrypt / Chiffrer
 export function encrypt(data) {
   return CryptoJS.AES.encrypt(data, SECRET).toString();
 }
 
-// Decrypt / Déchiffrer
+// AES decrypt / Déchiffrer
 export function decrypt(data) {
   const bytes = CryptoJS.AES.decrypt(data, SECRET);
   return bytes.toString(CryptoJS.enc.Utf8);
+}
+
+// SHA256 hash for passwords / Hash SHA256 pour mot de passe
+export function hashPassword(password) {
+  return CryptoJS.SHA256(password).toString();
 }
