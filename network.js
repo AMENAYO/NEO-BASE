bc29cc4de38fd6846e942b3c304f5785a6a1f6fe
AMENAYO <AMENAYO@proton.me>
2026-02-28 12:00:59 +0100

    Create network.js

---
+++ network.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+export async function discoverPeers(config, peers) {
+  for (const peer of config.bootstrapPeers) {
+    try {
+      const res = await axios.get(`${peer}/peers`);
+      res.data.forEach(p => peers.add(p));
+      peers.add(peer);
+    } catch {}
+  }
+}
+
+export async function broadcast(peers, path, data) {
+  for (const peer of peers) {
+    try {
+      await axios.post(`${peer}${path}`, data);
+    } catch {}
+  }
+}
